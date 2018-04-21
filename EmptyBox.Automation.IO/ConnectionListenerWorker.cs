d301ae19af8c18f893342905008b84682f6ffb13
Dj Art <art.letter.mail@gmail.com>
2018-04-22 01:08:45 +0800

    init

---
+++ EmptyBox.Automation.IO/ConnectionListenerWorker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using EmptyBox.IO.Network;
+
+namespace EmptyBox.Automation.Network
+{
+    public sealed class ConnectionListenerWorker : Pipeline<IConnectionListener, IConnection>, IPipelineIO<IConnectionListener, IConnection>
+    {
+        event EventHandler<IConnection> IPipelineOutput<IConnection>.Output { add => Output += value; remove => Output -= value; }
+
+        EventHandler<IConnectionListener> IPipelineInput<IConnectionListener>.Input => async (sender, output) =>
+        {
+            Handlers.Add(output);
+            output.ConnectionSocketReceived += ConnectionSocketReceived;
+            SocketOperationStatus status = await output.Start();
+            switch (status)
+            {
+                case SocketOperationStatus.Success:
+                    break;
+                default:
+                    Handlers.Remove(output);
+                    output.ConnectionSocketReceived -= ConnectionSocketReceived;
+                    break;
+            }
+        };
+
+        private List<IConnectionListener> Handlers;
+
+        private event EventHandler<IConnection> Output;
+        
+        public ConnectionListenerWorker()
+        {
+            Handlers = new List<IConnectionListener>();
+        }
+
+        private void ConnectionSocketReceived(IConnectionListener handler, IConnection socket)
+        {
+            Output?.Invoke(this, socket);
+        }
+    }
+}
---
+++ EmptyBox.Automation.IO/ConnectionWorker.cs
@@ -0,0 +1,65 @@
+﻿using EmptyBox.IO.Network;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EmptyBox.Automation.IO
+{
+    public class ConnectionWorker : Pipeline<(IConnection Connection, byte[] Message), (IConnection Connection, byte[] Message)>, IPipelineIO<(IConnection Connection, byte[] Message), (IConnection Connection, byte[] Message)>, IPipelineInput<IConnection>
+    {
+        EventHandler<IConnection> IPipelineInput<IConnection>.Input => (x, y) =>
+        {
+            y.ConnectionInterrupt += ConnectionInterrupt;
+            y.MessageReceived += MessageReceived;
+            Notify_Connected.Send(y);
+        };
+
+        EventHandler<(IConnection Connection, byte[] Message)> IPipelineInput<(IConnection Connection, byte[] Message)>.Input => async (x, y) =>
+        {
+            SocketOperationStatus status = await y.Connection.Send(y.Message);
+            switch (status)
+            {
+                case SocketOperationStatus.Success:
+                    break;
+                default:
+                    Notify_SendFailed.Send(y);
+                    break;
+            }
+        };
+
+        event EventHandler<(IConnection Connection, byte[] Message)> IPipelineOutput<(IConnection Connection, byte[] Message)>.Output
+        {
+            add => Output += value;
+            remove => Output -= value;
+        }
+
+        private event EventHandler<(IConnection Connection, byte[] Message)> Output;
+        
+        public ExternalInput<IConnection> Notify_Connected { get; private set; }
+        public ExternalInput<IConnection> Notify_Disconnected { get; private set; }
+        public ExternalInput<(IConnection Connection, byte[] Message)> Notify_SendSuccess { get; private set; }
+        public ExternalInput<(IConnection Connection, byte[] Message)> Notify_SendFailed { get; private set; }
+
+        public ConnectionWorker()
+        {
+            Notify_Connected = new ExternalInput<IConnection>();
+            Notify_Disconnected = new ExternalInput<IConnection>();
+            Notify_SendFailed = new ExternalInput<(IConnection Connection, byte[] Message)>();
+            Notify_SendSuccess = new ExternalInput<(IConnection Connection, byte[] Message)>();
+        }
+
+        private void MessageReceived(IConnection connection, byte[] message)
+        {
+            Output?.Invoke(this, (connection, message));
+        }
+
+        private void ConnectionInterrupt(IConnection connection)
+        {
+            connection.MessageReceived -= MessageReceived;
+            connection.ConnectionInterrupt -= ConnectionInterrupt;
+            Notify_Disconnected.Send(connection);
+        }
+    }
+}
